a09eeeb55a1f230adfb7cbe1834329432b0808d4
Nicolas Morel <nmr.morel@gmail.com>
2012-11-14 23:28:31 +0100

    added dependencies : logging, guava, spring, jersey

---
+++ homework-api/src/main/java/com/github/nmorel/homework/api/resources/TestResources.java
@@ -0,0 +1,24 @@
+package com.github.nmorel.homework.api.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path( "test" )
+public class TestResources
+{
+    private static final Logger logger = LoggerFactory.getLogger( TestResources.class );
+
+    @GET
+    @Path( "{name}" )
+    public String get( @PathParam( "name" ) String name )
+    {
+        logger.debug( "received : {}", name );
+        return "Hello " + name + "!";
+    }
+}
